2c5dc3bb52e39d7b451ee3a8e3e459081c6b7e1e
solfeier <49317121@est.ort.edu.ar>
2026-06-11 14:51:12 -0300

    Update results display in results_screen.dart

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -22,27 +22,44 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+
             const Text(
               "Resultados del Análisis:",
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
+            
             const SizedBox(height: 20),
+            
             const Text("Producto más caro:", style: TextStyle(fontWeight: FontWeight.bold)),
+            
             Text("Nombre: ${masCaro.name} - Descripción: ${masCaro.description}"),
+            
             const SizedBox(height: 15),
+            
             const Text("Producto más barato:", style: TextStyle(fontWeight: FontWeight.bold)),
+            
             Text("Nombre: ${masBarato.name} - Descripción: ${masBarato.description}"),
+            
             const SizedBox(height: 15),
+            
             const Text("Mayor cantidad:", style: TextStyle(fontWeight: FontWeight.bold)),
+            
             Text("Nombre: ${mayorCantidad.name} - Descripción: ${mayorCantidad.description}"),
+            
             const SizedBox(height: 15),
+            
             const Text("Menor cantidad:", style: TextStyle(fontWeight: FontWeight.bold)),
+            
             Text("Nombre: ${menorCantidad.name} - Descripción: ${menorCantidad.description}"),
+            
             const SizedBox(height: 20),
+            
             const Divider(),
+            
             const SizedBox(height: 10),
+            
             Text(
-              "Precio promedio total: \$${promedio.toStringAsFixed(2)}",
+            "Precio promedio total: $promedio",
               style: const TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
